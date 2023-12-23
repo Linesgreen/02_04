764c7126a685a9e43d9fa270806a5ff85a70fb0b
Vladislav Evg <linesgreen@mail.ru>
2023-12-23 19:43:37 +0300

    add elaticEmail 2.0 http

--- src/adapters/email-adapter.ts
+++ src/adapters/email-adapter.ts
@@ -1,9 +1,16 @@
 // noinspection UnnecessaryLocalVariableJS
 
 import nodemailer from "nodemailer";
-
+import {
+    BodyContentType,
+    BodyPart,
+    Configuration,
+    EmailMessageData,
+    EmailsApi
+} from "@elasticemail/elasticemail-client-ts-axios";
+import {contentValidation} from "../middlewares/post/postsValidator";
 export const emailAdapter = {
-    async sendEmail(email: string, subject: string, message: string) {
+    async sendEmail1(email: string, subject: string, message: string) {
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             port: 587,
@@ -19,8 +26,60 @@ export const emailAdapter = {
             to: email,
             subject: subject,
             html: message
-
         });
         return info
     },
+    async sendEmail(email: string, subject: string, message: string) {
+        const config = new Configuration({
+            apiKey: "1C0DFCCC4F5F94535C54F109E5FD57E6718E0E2DAA60FE02F52D72CFACF9F09ECC2661E271C8485FC61D73A0B7513D06"
+        });
+        const emailsApi = new EmailsApi(config);
+        const emailMessageData: EmailMessageData = {
+            Recipients: [
+                {
+                    Email: email,
+                    Fields: {
+                        name: "Name"
+                    }
+                }
+            ],
+            Content: {
+                Body: [
+                    {
+                        ContentType: BodyContentType.Html,
+                        Charset: "utf-8",
+                        Content: message
+                    } as BodyPart
+                ],
+                From: 'Vlad_Nyah <linesgreen@gmail.com>',
+                Subject: subject
+            }
+        };
+
+        emailsApi.emailsPost(emailMessageData).then((response) => {
+        }).catch((error) => {
+            console.error(error, "***********************************************");
+        });
+
+        return true
+    },
+    async sendEmail4(email: string, subject: string, message: string) {
+        const axios = require('axios');
+
+        const params = new URLSearchParams();
+        params.append('apikey', '1C0DFCCC4F5F94535C54F109E5FD57E6718E0E2DAA60FE02F52D72CFACF9F09ECC2661E271C8485FC61D73A0B7513D06');
+        params.append('subject', subject);
+        params.append('from', 'linesgreen@gmail.com');
+        params.append('to', email);
+        params.append('bodyHtml', message);
+        try {
+            const response = await axios.post('https://api.elasticemail.com/v2/email/send', params);
+            console.log('Email sent:', response.data);
+        } catch (error) {
+            console.error('Error sending email:', error);
+            return false
+        }
+
+        return true
+    }
 };
